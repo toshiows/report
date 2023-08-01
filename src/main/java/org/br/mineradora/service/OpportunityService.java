9eff7596fb1fcf08ae4ac20a0392c157351eaae7
toshiows <toshio_ws@hotmail.com>
2023-08-01 01:54:21 -0300

    Kafka event class and service class

---
+++ src/main/java/org/br/mineradora/service/OpportunityService.java
@@ -0,0 +1,20 @@
+package org.br.mineradora.service;
+
+import java.util.List;
+
+import org.br.mineradora.dto.OpportunityDTO;
+import org.br.mineradora.dto.ProposalDTO;
+import org.br.mineradora.dto.QuotationDTO;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public interface OpportunityService {
+
+	void buildOpportunity(ProposalDTO proposal);
+	
+	void saveQuotation(QuotationDTO quotation);
+	
+	List<OpportunityDTO> generateOpportunityData();
+	
+}
